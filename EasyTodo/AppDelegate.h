0a13ea8459c9513fb098819a7addf40bd925a7fe
lichang0716 <lichang.android@gmail.com>
2016-09-18 19:49:35 +0800

    init project

---
+++ EasyTodo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyTodo
+//
+//  Created by pwnlc on 16/9/17.
+//  Copyright © 2016年 xyz.pwnlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyTodo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  EasyTodo
+//
+//  Created by pwnlc on 16/9/17.
+//  Copyright © 2016年 xyz.pwnlc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *ItemFlagSwitch;
+
+@end
+
